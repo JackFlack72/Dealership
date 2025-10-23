b7290c733764daa42c7e1bfe4ee1b7016be6eb6a
lucas.rappatta24@outlook.com <lucas.rappatta24@outlook.com>
2025-10-23 16:04:28 -0500

    add main to Program and create field members for each class

--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -1,4 +1,12 @@
 package com.pluralsight;
 
 public class Vehicle {
+    private int vin;
+    private int year;
+    private String make;
+    private String model;
+    private String vehicleType; // car, truck, etc.
+    private String color;
+    private int odometer;
+    private double price;
 }
