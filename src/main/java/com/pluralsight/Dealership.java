d29c8710a9758e4d2d436aa624c00ba1329111d4
lucas.rappatta24@outlook.com <lucas.rappatta24@outlook.com>
2025-10-24 10:56:47 -0500

    add UML class diagram skeleton

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -1,10 +1,78 @@
 package com.pluralsight;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Dealership {
     private String name;
     private String address;
     private String phone;
-    ArrayList<Vehicle> inventory = new ArrayList<>();
+    private ArrayList<Vehicle> inventory;
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public List<Vehicle> getVehiclesByPrice() {
+
+    }
+
+    public List<Vehicle> getVehiclesByMakeModel() {
+
+    }
+
+    public List<Vehicle> getVehiclesByYear() {
+
+    }
+
+    public List<Vehicle> getVehiclesByColor() {
+
+    }
+
+    public List<Vehicle> getVehiclesByMileage() {
+
+    }
+
+    public List<Vehicle> getVehiclesByType() {
+
+    }
+
+    public List<Vehicle> getAllVehicles() {
+
+    }
+
+    public void addVehicle(Vehicle vehicle) {
+
+    }
+
+    public void removeVehicle(Vehicle vehicle) {
+
+    }
+
 }
--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -2,4 +2,50 @@ package com.pluralsight;
 
 public class UserInterface {
     Dealership dealership = new Dealership();
+
+    public void UserInterface() {
+
+    }
+
+    public void display() {
+
+    }
+
+    public void GetByPriceRequest() {
+
+    }
+
+    public void GetByMakeModelRequest() {
+
+    }
+
+    public void GetByYearRequest() {
+
+    }
+
+    public void GetByColorRequest() {
+
+    }
+
+    public void GetByMileageRequest() {
+
+    }
+
+    public void GetByVehicleTypeRequest() {
+
+    }
+
+    public void GetAllVehiclesRequest() {
+
+    }
+
+    public void AddVehicleRequest() {
+
+    }
+
+    public void RemoveVehicleRequest() {
+
+    }
+
+
 }
